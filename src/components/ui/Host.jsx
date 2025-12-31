cc8e330e7ef2284391e1f6868aa7a4f8b822657c
MTDev2024 <mt.dev.2023@gmail.com>
2025-12-31 02:46:56 +0100

    refactor(Host): déstructuration des props et sécurisation du nom de l’hôte

--- src/components/ui/Host.jsx
+++ src/components/ui/Host.jsx
@@ -1,7 +1,16 @@
-export default function Host({ name, picture }) {
+export default function Host({ name = "", picture }) {
+  // Découpage nom complet en prénom + nom
+  // "Alex Dupont" → ["Alex", "Dupont"]
+  const [firstName, lastName] = name.split(" ");
+
   return (
     <div className="host">
-      <p className="host__name">{name}</p>
+      <p className="host__name">
+        {firstName}
+        <br />
+        {lastName}
+      </p>
+
       <img src={picture} alt={name} className="host__picture" />
     </div>
   );
